50c8c8fe7c580d959c55f5018731e8eda888857e
rahul-singh-30066013 <30066013@adani.com>
2022-05-17 13:15:49 +0530

    show view

--- lib/first_route.dart
+++ lib/first_route.dart
@@ -1,4 +1,7 @@
+import 'package:ads_library/assets/json/ads_big_banner.dart';
 import 'package:ads_library/assets/json/ads_dialog_banner.dart';
+import 'package:ads_library/assets/json/ads_single_banner.dart';
+import 'package:ads_library/assets/json/ads_small_banner.dart';
 import 'package:ads_library/loyalty_dialog/ads_dialog_screen.dart';
 import 'package:ads_library/router_navigation/routes_constants.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -30,16 +33,103 @@ class _FirstRouteState extends State<FirstRoute> {
           future: getData(),
           builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              final DialogBanner dialogBanner = widget.viewModel.adResponseState.data as DialogBanner;
-              return Center(
-                child: ElevatedButton(
-                    child:  Text(dialogBanner.fields?.first.title ?? 'Open Dialog'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
-                      shape: const StadiumBorder(),
+              final value = widget.viewModel;
+              switch (widget.type) {
+                case AdsType.dialog:
+                  final DialogBanner adsDialogBanner = value.adResponseState.data as DialogBanner;
+                  return Center(
+                    child: ElevatedButton(
+                        child:  Text(adsDialogBanner.widgetType ?? 'Open Dialog'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blue,
+                          shape: const StadiumBorder(),
+                        ),
+                        onPressed: () => AdsDialogScreen().showBigBanner(context)),
+                  );
+                case AdsType.singleBanner:
+                  final SingleBanner singleBanner = value.adResponseState.data as SingleBanner;
+                  return Container(
+                      margin: const EdgeInsets.all(8),
+                      height: 54.0,
+                      width: 220,
+                      color: Colors.blue[100],
+                      child: Image.network(singleBanner.fields?.first.imageLink ?? '', fit: BoxFit.fill,));
+                case AdsType.bigBanner:
+                  final BigBanner bigBanner = value.adResponseState.data as BigBanner;
+                  return SizedBox(
+                    height: 240.0,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: List.generate(bigBanner.fields?.length ?? 0, (int index) {
+                        return Container(
+                          width: 220,
+                          height: 240.0,
+                          margin: const EdgeInsets.all(10.0),
+                          child: ClipRect(
+                            child: Banner(
+                              message: "20% off !!",
+                              location: BannerLocation.bottomStart,
+                              color: Colors.red,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.blue[200],
+                                    borderRadius:
+                                    BorderRadius.circular(8)),
+                                child: Column(
+                                  children: <Widget>[
+                                    const SizedBox(
+                                      height: 6,
+                                    ),
+                                    Image.network(bigBanner.fields?[index].imageLink ?? ''),
+                                    //Image.network
+                                    Text(
+                                      bigBanner.fields?[index].title ?? '',
+                                      style: const TextStyle(
+                                          color: Colors.green,
+                                          fontSize: 20,
+                                          fontWeight:
+                                          FontWeight.bold), //TextStyle
+                                    ),
+                                    //Text
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    ElevatedButton(
+                                        child: const Text('Book Now'),
+                                        style: ElevatedButton.styleFrom(
+                                          primary: Colors.blue,
+                                          shape: const StadiumBorder(),
+                                        ),
+                                        onPressed: () => AdsDialogScreen().showBigBanner(context))
+                                    //RaisedButton
+                                  ], //<Widget>[]
+                                ), //Column
+                              ), //Padding
+                            ), //Container
+                          ), //Banner
+                        );
+                      }),
                     ),
-                    onPressed: () => AdsDialogScreen().showBigBanner(context)),
-              );
+                  );
+                case AdsType.smallBanner:
+                  final SmallBanner smallBanner = value.adResponseState.data as SmallBanner;
+                  return SizedBox(
+                    height: 150.0,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: List.generate(smallBanner.fields?.length ?? 0, (int index) {
+                        return Card(
+                          color: Colors.blue[index * 100],
+                          child: SizedBox(
+                            width: 100.0,
+                            height: 100.0,
+                            child: Image.network(smallBanner.fields?[index].imageLink ?? ''),
+                          ),
+                        );
+                      }),
+                    ),
+                  );
+              }
             } else if (snapshot.connectionState == ConnectionState.none) {
               return const Text("No data");
             }
@@ -51,6 +141,6 @@ class _FirstRouteState extends State<FirstRoute> {
 
   Future<DataSnapshot> getData() async {
     await Firebase.initializeApp();
-    return  widget.viewModel.fetchDataFromFirebase(widget.type);
+    return widget.viewModel.fetchDataFromFirebase(widget.type);
   }
 }
