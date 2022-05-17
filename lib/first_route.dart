29219c7869355a1f374c8de6d0bccf80b2fb2d29
rahul-singh-30066013 <30066013@adani.com>
2022-05-17 16:08:41 +0530

    init firebase

--- lib/first_route.dart
+++ lib/first_route.dart
@@ -4,6 +4,7 @@ import 'package:ads_library/assets/json/ads_single_banner.dart';
 import 'package:ads_library/assets/json/ads_small_banner.dart';
 import 'package:ads_library/loyalty_dialog/ads_dialog_screen.dart';
 import 'package:ads_library/router_navigation/routes_constants.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
@@ -139,6 +140,7 @@ class _FirstRouteState extends State<FirstRoute> {
 
 
   Future<DataSnapshot> getData() async {
+    await Firebase.initializeApp();
     return widget.viewModel.fetchDataFromFirebase(widget.type);
   }
 }
